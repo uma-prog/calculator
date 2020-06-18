a1eb3e321744ec06c32862e52de3d3a8e64a84fb
uma-prog <52752663+uma-prog@users.noreply.github.com>
2020-06-18 22:28:56 +0530

    Create calculator.css

---
+++ calculator.css
@@ -0,0 +1,23 @@
+body {
+    margin: 30px;
+    color:red;
+    background-color: black;
+    font-size: 20px;
+   
+}
+
+input[type=text] {
+  background-color: white;
+  color: black;
+    font-size: 20px;
+}
+
+input[type=button]{
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-decoration: none;
+  margin: 4px 2px;
+  cursor: pointer;
+}
